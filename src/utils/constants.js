3ec3fd0af38c5fd803e15eff3e725cb971396055
gzlj3 <gzlj3@163.com>
2018-10-10 21:36:33 +0800

    home

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,6 +1,9 @@
 // page当前状态
-export const PAGE_QUERY = 0;    // 查看状态
-export const PAGE_NEW = 1;      // 新增状态
-export const PAGE_UPDATED = 2;  // 修改状态
-export const PAGE_DELETE = 3;   // 删除状态
+export const PAGE_QUERY = 0; // 查看状态
+export const PAGE_NEW = 1; // 新增状态
+export const PAGE_UPDATED = 2; // 修改状态
+export const PAGE_DELETE = 3; // 删除状态
 
+// 后台处理返回状态
+export const SUCCESS = 0; // 处理成功
+export const FAILURE = 1; // 处理错误
